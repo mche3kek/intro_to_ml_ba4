322e4da024b81a043d256e07fe5479896b3a3de3
Mamoun <mamoun.imghi@epfl.ch>
2023-06-02 15:06:06 +0200

    Upload Milestone 2

---
+++ 346722_346195_345616_project_ms1/src/methods/kmeans.py
@@ -0,0 +1,133 @@
+import numpy as np
+
+
+class KMeans(object):
+    """
+    K-Means clustering class.
+
+    We also use it to make prediction by attributing labels to clusters.
+    """
+
+    def __init__(self, K, max_iters=100):
+        """
+        Initialize the new object (see dummy_methods.py)
+        and set its arguments.
+
+        Arguments:
+            K (int): number of clusters
+            max_iters (int): maximum number of iterations
+        """
+        self.K = K
+        self.max_iters = max_iters
+
+    def k_means(self, data, max_iter=100):
+        """
+        Main K-Means algorithm that performs clustering of the data.
+        
+        Arguments: 
+            data (array): shape (N,D) where N is the number of data samples, D is number of features.
+            max_iter (int): the maximum number of iterations
+        Returns:
+            centers (array): shape (K,D), the final cluster centers.
+            cluster_assignments (array): shape (N,) final cluster assignment for each data point.
+        """
+
+        # We define K as the number of our clusters
+        K = self.K
+        # 1) Initialize the centers :
+        # Select the first K random index
+        random_idx = np.random.permutation(data.T.shape[0])
+        # Use these index to select centers from data
+        centers = data[random_idx[:K]]
+
+        # 2) Loop over the iterations
+        for i in range(max_iter):
+            # We keep in memory the centers of the previous iteration
+            old_centers = centers.copy()  
+
+            # We assign each data to its corresponding cluster
+            cluster_assignments = self.assign_cluster(data, centers)
+
+            # We compute the new centers 
+            for k in range(K):
+                centers[k] = np.mean(data[cluster_assignments == k], axis=0)
+
+            # End of the algorithm if the centers have converged
+            if (np.all(old_centers == centers)): 
+                break
+        
+        return centers, cluster_assignments
+    
+    def fit(self, training_data, training_labels):
+        """
+        Train the model and return predicted labels for training data.
+
+        You will need to first find the clusters by applying K-means to
+        the data, then to attribute a label to each cluster based on the labels.
+        
+        Arguments:
+            training_data (array): training data of shape (N,D)
+            training_labels (array): labels of shape (N,)
+        Returns:
+            pred_labels (array): labels of shape (N,)
+        """
+
+        # We call the KMeans algorithm on our training data
+        centers, cluster_assignments = self.k_means(training_data)
+
+        # We use voting to attribute a label to each cluster center.
+        cluster_center_label = np.zeros(centers.shape[0])
+        for i in range(len(centers)):
+            label = np.argmax(np.bincount(training_labels[cluster_assignments == i]))
+            cluster_center_label[i] = label
+
+        # We save our centers and our cluster to label mapping to use them for future data
+        self.centers = centers
+        self.cluster_center_label = cluster_center_label
+
+        return self.predict(training_data)
+
+    def predict(self, test_data):
+        """
+        Runs prediction on the test data given the cluster center and their labels.
+
+        To do this, first assign data points to their closest cluster, then use the label
+        of that cluster as prediction.
+        
+        Arguments:
+            test_data (np.array): test data of shape (N,D)
+        Returns:
+            pred_labels (np.array): labels of shape (N,)
+        """
+
+        # We assign each data to its corresponding cluster
+        cluster_assignments = self.assign_cluster(test_data, self.centers)
+
+        # We convert cluster index to label
+        pred_labels = self.cluster_center_label[cluster_assignments]
+
+        return pred_labels
+    
+    def assign_cluster(self, data, centers):
+        """
+        Computes the Euclidean distance of each data to the clusters and assign it to the corresponding one.
+        
+        Arguments:
+            data (np.array): data of shape (N,D)
+        Returns:
+            cluster_assignments (array): shape (N,) cluster assignment for each data point.
+        """
+
+        # We define N and K as the size of our data set and of our clusters respectively
+        N = data.shape[0]
+        K = self.K
+
+        # Here, we will loop over the cluster
+        distances = np.zeros((N, K))
+        for k in range(K):
+            # Compute the euclidean distance for each data to each center
+            center = centers[k]
+            distances[:, k] = np.sqrt(((data - center) ** 2).sum(axis=1))
+
+        # We assign each data to its closest cluster 
+        return np.argmin(distances, axis=1)
---
+++ 346722_346195_345616_project_ms1/src/methods/logistic_regression.py
@@ -0,0 +1,106 @@
+import numpy as np
+
+from ..utils import get_n_classes, label_to_onehot, onehot_to_label
+
+
+class LogisticRegression(object):
+    """
+    Logistic regression classifier.
+    """
+
+    def __init__(self, lr, max_iters=500):
+        """
+        Initialize the new object (see dummy_methods.py)
+        and set its arguments.
+
+        Arguments:
+            lr (float): learning rate of the gradient descent
+            max_iters (int): maximum number of iterations
+        """
+        self.lr = lr
+        self.max_iters = max_iters
+
+        
+
+    def fit(self, training_data, training_labels):
+        """
+        Trains the model, returns predicted labels for training data.
+
+        Arguments:
+            training_data (array): training data of shape (N,D)
+            training_labels (array): regression target of shape (N,)
+        Returns:
+            pred_labels (array): target of shape (N,)
+        """
+        D = training_data.shape[1]  # number of features
+        C = get_n_classes(training_labels)  # number of classes
+        # Random initialization of the weights
+        self.weights = np.random.normal(0, 0.1, (D, C))
+        for it in range(self.max_iters):
+            gradient = self.gradient_logistic(training_data, label_to_onehot(training_labels)) # We compute the gradient
+            self.weights = self.weights - self.lr * gradient # Then compute the new weightts
+        
+        return self.predict(training_data)
+
+    def predict(self, test_data):
+        """
+        Runs prediction on the test data.
+        
+        Arguments:
+            test_data (array): test data of shape (N,D)
+        Returns:
+            pred_labels (array): labels of shape (N,)
+        """
+        probs = self.f_softmax(test_data) # Compute the probabilities
+        pred_labels = onehot_to_label(probs) # Compute the predicted label
+       
+        return pred_labels
+    
+    def f_softmax(self, data):
+        """
+        Softmax function
+        
+        Args:
+            data (array): Input data of shape (N, D)
+        Returns:
+            array of shape (N, C): Probability array where each value is in the
+                range [0, 1] and each row sums to 1.
+                The row i corresponds to the prediction of the ith data sample, and 
+                the column j to the jth class. So element [i, j] is P(y_i=k | x_i, W)
+        """
+
+        w_exp = np.exp(data @ self.weights) # Here we compute exponential of between a x vector and its corresponding weight
+
+        exp_sum = np.sum(w_exp, axis = 1) # We compute the total of all exponentials
+
+        exp_sum = exp_sum.reshape([exp_sum.shape[0], 1]) # We reshape it in the right shape
+
+        y = w_exp / exp_sum # Finally we compute the probability
+    
+        return y
+    
+    def loss_logistic(self, data, labels):
+        """ 
+        Loss function for multi class logistic regression, i.e., multi-class entropy.
+        
+        Args:
+            data (array): Input data of shape (N, D)
+            labels (array): Labels of shape  (N, C)  (in one-hot representation)
+        Returns:
+            float: Loss value 
+        """
+        
+        return -np.sum(np.log(self.f_softmax(data)) * labels) # We simply compute the loss according to the formula
+    
+    def gradient_logistic(self, data, labels):
+        """
+        Compute the gradient of the entropy for multi-class logistic regression.
+        
+        Args:
+            data (array): Input data of shape (N, D)
+            labels (array): Labels of shape  (N, C)  (in one-hot representation)
+        Returns:
+            grad (np.array): Gradients of shape (D, C)
+        """
+        
+        return data.T @ (self.f_softmax(data) - labels) # We compute the gradient of E(W) for the full weight matrix
